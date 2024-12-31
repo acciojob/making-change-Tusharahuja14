b78b1629f31b821e60bbbe656bf9e2e569d94bfe
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2024-12-31 14:12:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const makeChange = (c) => {
 	obj.q=Math.floor(c/25);
 	c%=25;
 	obj.d=Math.floor(c/10);
-	d%=10;
-	obj.n=Math.floor(n/5);
-	n%=5;
+	c%=10;
+	obj.n=Math.floor(c/5);
+	c%=5;
 	obj.p=c;
 	
 	return obj;
 };
 
 //Do not the change the code below
-const c = prompt("Enter c: ");
+/*const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
